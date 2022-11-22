e11b546769038c1fc94a66de21c6fe6d132c66f1
Alex <glock-alexander@t-online.de>
2022-11-22 15:06:29 +0100

    Abgabe HU4?

--- NumGewDGL/HU4_Stabilitaet.m
+++ NumGewDGL/HU4_Stabilitaet.m
@@ -2,23 +2,29 @@
 % Code von Alexander Glock
 clearvars
 
-% Vorgabe der AWP Dimension A_awp dim := (n_awp+1)x(n_awp+1)
-n_awp=50;
-
-% Anfangswertproblem Paramter: 
+% Vorgabe der Matrix Dimension des AWP := (n_awp+1)x(n_awp+1)
+n_awp=30;
+% Anfangswertproblem generieren: 
 % --> make_task1(n) = System aus Aufgabenteil (I)
 % --> make_task2(n) = System aus Aufgabenteil (II)
 [A_awp,y0,t0,T]=make_task2(n_awp); %make_task2(n)
+
+% ODE der Form:
 f = @(t,y) (-1)*A_awp*y;
 
 % Verfahrensauswahl mit make_RK(sel):
-% sel: 1=Heun (a)   2=Gauss-2 (b)   3=imp. Trapez (c) 
-[A,b,g]=make_RK(3);
+% sel: 1=Heun a)   2=Gauss-2 b)   3=imp. Trapez c) 
+[A,b,g]=make_RK(2);
 
-% experimentelle Ordnungsermittlung:
-test = convPlot(y0,f,t0,T,A,b,g,A_awp);
+% analytische LSG vorbereiten:
+[EV, D] = eig(-A_awp);  % Eigenvektoren              
+E=diag(D);              % Eigenwerte
+C= linsolve(EV,y0);     % Konstanten mittels y(0) Bed.
 
+% experimentelle Ordnungsermittlung mit Plot:
+test = convPlot(y0,f,t0,T,A,b,g,EV,E,C);
 
+% schriftliche Ergebnisse ganz unten 
 function [A_awp,y0,t0,T] = make_task1(n)
     % A Diagonale erstellen - Dim n+1
     d0 = 2 + zeros(1, n+1);
@@ -76,38 +82,55 @@ function [A,b,g] = make_RK(sel)
             g =[0 1]';
     end
 end
-function dY = myODE(Y,A_awp)
-    dY= (-1)*A_awp*Y;
+function Y = Yex(C,E,EV,t)
+    Y=zeros(1,length(E))';
+    for k=(1:1:length(E))
+        Y_k= C(k)*EV(k)*exp(E(k)*t);
+        Y=Y+Y_k;
+    end
 end
-function test = convPlot(y0,f,t0,T,A,b,g,A_awp)
+function test = convPlot(y0,f,t0,T,A,b,g,EV,E,C)
+        h0=@(h) 1;
         h1=@(h) h;
-        h2=@(h) h.^2;
       
-        hmax = T/10;                    % maximale Zeitschrittweite
-        hmin = T/1000;                  % minimale Zeitschrittweite
-        hh = flip(hmin:15*hmin:hmax);   % Zeitschwrittgrößen von hmax bis hmin
-        cc = 0;                         % norm fehlervektor initialisieren
+        hmax = T/100;                    % maximale Zeitschrittweite
+        hmin = T/10000;                  % minimale Zeitschrittweite
+        hh = flip(hmin:10*hmin:hmax);    % Zeitschwrittgrößen von hmax bis hmin
+        cc = 0;                          % norm fehlervektor initialisieren
 
     for h=hh(2:end)
         n=round(T/h);
         % Approximation mit RKV
         [tt, yy] = myRK(y0,f,t0,T,n,A,b,g);
-        % "exakte" Lösung des DGL Systems mit ODE berechnen;
-        sol=ode45(@(t,Y) myODE(Y,A_awp),tt,y0);%[~, yyex]
-        [yyex,~]=deval(sol,tt,1);
+        % "exakte" Lösung des DGL Systems zu tt berechnen:
+        yyex=Yex(C,E,EV,tt);
 
         % norm. Fehler des RKV
-        c = norm((yyex-yy(1,:)),'Inf');
+        c = norm((yyex(1,:)-yy(1,:)),'Inf');
         cc = [cc,c];
     end
     
     % Konvergenz plot
-    loglog(hh,cc,'-r',hh,h1(hh),'--b',hh,h2(hh),'--g')
+    loglog(hh,cc,'-r',hh,h0(hh),'--b',hh,h1(hh),'--g')
     title('Numerical error vs Stepwidth in double log scale')
     xlabel('size of h')
     ylabel('Numerical error || y-y_h ||')
-    legend('Num error','lin. konv','quad. konv.')
+    legend('Num error','konst 1','lin. konv')
     grid on
 
     test='fertig';
 end
+
+%% Ergebnis:
+% Mit den gegebenen Anfangswertproblemen besitzen alle drei Verfahren die
+% Konvergenzordnung 0, da sie nicht konvergieren und damit einen konstanten
+% Fehler zur analytischen Lösung vorweisen der niemals verschwindet.
+
+% Die impliziten Verfahren (b & c) sind für alle Schrittweiten h stabil.
+% Das äußert sich im Konvergenzplot durch einen gleichmäßigen Verlauf des numerischen
+% Fehlers mit endlicher Steigung. Beim expliziten Verfahren (a) entwickelt
+% sich ab einem h von ungefähr 10^(-2) eine Instabilität. Zu sehen ist das am
+% numerische Fehler der an diesem Punkt im Konvergenzplot rasant ansteigt und sehr groß wird.
+
+
+
