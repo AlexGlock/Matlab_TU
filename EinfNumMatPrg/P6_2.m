76ebf72ee5f9b0ea48a2d88479fb02d063094497
Alex <glock-alexander@t-online.de>
2023-01-31 01:30:10 +0100

    P6_1

---
+++ EinfNumMatPrg/P6_2.m
@@ -0,0 +1,11 @@
+%% Aufgabe P6.2 - Gruppe 7 - Alexander Glock, Jannis Röder
+
+clearvars
+a = 0;
+b = 1;
+n = 100;
+
+% a) function [w,x] = trapezregel()
+
+
+% a) function R = rombergQuadratur(f,a,b,n,q)
